49e54eede3e00f82cd32e256675699e5796bc239
zgtm <zgtm@example.com>
2026-03-25 11:28:55 +0100

    Change version to 0.0.8

--- release.sh
+++ release.sh
@@ -6,8 +6,8 @@ if [ -z "$1" ]; then
 fi
 
 sed -i 's/^version = ".*"$/version = "'$1'"/' Cargo.toml
-cargo run
-cargo publish
+cargo build
 git commit -am "Change version to $1"
+cargo publish
 git tag -a v$1 -m "Version $1"
 git push
